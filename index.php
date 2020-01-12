d83baf18effd20b0f93308cbcf7252ffc6c2d42b
Meliisa9 <melisa_lolz@hotmail.com>
2020-01-12 16:25:10 +0100

    add files

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-91110704-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'UA-91110704-2');
+    </script>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <title>#1 Counter-Strike Zombie Server | Elite-Hunterz</title>
+    <meta content="Join NOW our Counter-Strike Source Zombie server, we are the #1 most played game-server available in Europe and Russia."
+          name="description"/>
+    <link href="https://elite-hunterz.com/" rel="canonical"/>
+    <link href="css/index.css" rel="stylesheet" >
+	<link rel="stylesheet" href="@import url(http://fonts.googleapis.com/css?family=Capriola);">
+    <link href="https://fonts.googleapis.com/css?family=Tangerine:bold,bolditalic|Inconsolata:italic|Droid+Sans"
+          rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div class="header-wrap"><!-- motd -->
+    <header class="container_16">
+        <a href="https://elite-hunterz.com"><h1 class="logo"></h1></a>
+        <center>
+            <div id='cssmenu'>
+<ul>
+   <li class='active'><a href='https://elite-hunterz.com/' data-title="Return to the homepage"><span>Home</span></a></li>
+   <li><a href='https://forum.elite-hunterz.com/' data-title="Visit the forum"><span>Forum</span></a></li>
+   <li><a href='https://bans.elite-hunterz.info/' data-title="Find Bans and Comms here"><span>Bans</span></a></li>
+   <li><a href='https://stats.elite-hunterz.info/' data-title="Find your rank here"><span>Your Rank</span></a></li>
+   <li><a href='https://forum.elite-hunterz.com/donate/' data-title="Make a contibution to the community, perks included"><font color="#00FF00"><span>Premium</span></a></li></font>
+   <li><a href='https://forum.elite-hunterz.com/pages/gameservers/' data-title="A list of all our servers"><span>Servers</span></a></li>
+   <li><a href='https://forum.elite-hunterz.com/applicationform/navtab' data-title="Apply to become a part of the team"><span>Get Admin</span></a></li>
+   <li class='last'><a href='https://discord.gg/AdRxXCA' data-title="Press here to join our discord server"><span>Discord</span></a></li>
+</ul>
+</div></div></header>
+        </center>
+		
+		<br><br><br>
+		<div class="header-wrap">
+		<div class="container_69">
+		
+<div class="content" id="first"><ul>
+                        <font color="#00FF00">Elite-Hunterz is a friendly community that was founded on top of a game
+                            called Counter-Strike: Source!
+                            <br/>
+                            We are the only true Players of Zombie Gameservers and we're one of the few communities left
+                            in this game!<br/>
+                            If you're looking for a Friendly and Fun community to play on, then this is the right place!<br/>
+                            Enjoy your stay Gamer!<br/><br/>
+                        </font>
+                    </ul></div>
+<div class="content" id="second"><font color="#00FF00"><ul>
+                        <font color="#00FF00">Go-Free is a friendly CSGO dedicated community where you can surf and bhop, but also a place where you can sit back relax and have a nice chat.<br>
+						You can find your <font color="#FF0000"><a href="https://stats.go-free.info/">Go-Free Stats right here</a></font>.
+						
+                            <br/><br/><br/><br/>
+                        </font>
+                    </ul>
+
+</font></div>
+
+				</div></div>
+				<div class="content" id="third"><font color="#00FF00"><iframe src="https://discordapp.com/widget?id=310814516747173909&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe></font></div>
+				
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <ins class="adsbygoogle" style="display:inline-block;width:336px;height:280px"
+                         data-ad-client="ca-pub-3649218512429867" data-ad-slot="6001712336"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+</body>
+</html>
